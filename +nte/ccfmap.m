93907a78fa076b2bfdb51babfdf56487aa8ee342
Rob Campbell <git@raacampbell.com>
2023-01-04 12:59:09 +0000

    minor: formatting

--- +nte/ccfmap.m
+++ +nte/ccfmap.m
@@ -1,21 +1,21 @@
 function ccf_cmap = ccfmap
 
-% (copied from cortex-lab/allenCCF/setup_utils
-% too many adjacent areas have colors that are too similar
+    % (copied from cortex-lab/allenCCF/setup_utils
+    % too many adjacent areas have colors that are too similar
 
-allen_atlas_path = fileparts(which('template_volume_10um.npy'));
-if isempty(allen_atlas_path)
-    error('CCF atlas not in MATLAB path (click ''Set path'', add folder with CCF)');
-end
+    allen_atlas_path = fileparts(which('template_volume_10um.npy'));
+    if isempty(allen_atlas_path)
+        error('CCF atlas not in MATLAB path (click ''Set path'', add folder with CCF)');
+    end
 
-st = nte.load_structure_tree(fullfile(allen_atlas_path,'structure_tree_safe_2017.csv')); % a table of what all the labels mean
+    st = nte.load_structure_tree(fullfile(allen_atlas_path,'structure_tree_safe_2017.csv')); % a table of what all the labels mean
 
-% Create CCF colormap
-% (copied from cortex-lab/allenCCF/setup_utils
-ccf_color_hex = st.color_hex_triplet;
-ccf_color_hex(cellfun(@numel,ccf_color_hex)==5) = {'019399'}; % special case where leading zero was evidently dropped
-ccf_cmap_c1 = cellfun(@(x)hex2dec(x(1:2)), ccf_color_hex, 'uni', false);
-ccf_cmap_c2 = cellfun(@(x)hex2dec(x(3:4)), ccf_color_hex, 'uni', false);
-ccf_cmap_c3 = cellfun(@(x)hex2dec(x(5:6)), ccf_color_hex, 'uni', false);
-ccf_cmap = horzcat(vertcat(ccf_cmap_c1{:}),vertcat(ccf_cmap_c2{:}),vertcat(ccf_cmap_c3{:}))./255;
+    % Create CCF colormap
+    % (copied from cortex-lab/allenCCF/setup_utils
+    ccf_color_hex = st.color_hex_triplet;
+    ccf_color_hex(cellfun(@numel,ccf_color_hex)==5) = {'019399'}; % special case where leading zero was evidently dropped
+    ccf_cmap_c1 = cellfun(@(x)hex2dec(x(1:2)), ccf_color_hex, 'uni', false);
+    ccf_cmap_c2 = cellfun(@(x)hex2dec(x(3:4)), ccf_color_hex, 'uni', false);
+    ccf_cmap_c3 = cellfun(@(x)hex2dec(x(5:6)), ccf_color_hex, 'uni', false);
+    ccf_cmap = horzcat(vertcat(ccf_cmap_c1{:}),vertcat(ccf_cmap_c2{:}),vertcat(ccf_cmap_c3{:}))./255;
 
--- +nte/set_probe_endpoint.m
+++ +nte/set_probe_endpoint.m
@@ -1,57 +1,57 @@
 function set_probe_endpoint(h,eventdata,probe_atlas_gui)
 
-% Get guidata
-gui_data = guidata(probe_atlas_gui);
+    % Get guidata
+    gui_data = guidata(probe_atlas_gui);
 
-% Prompt for angles
-prompt_text = { ...
-    'AP position (mm from bregma)', ...
-    'ML position (mm from bregma)', ...
-    'DV position (mm from bregma)', ...
-    'Azimuth angle (relative to lambda -> bregma)', ....
-    'Elevation angle (relative to horizontal)'};
+    % Prompt for angles
+    prompt_text = { ...
+        'AP position (mm from bregma)', ...
+        'ML position (mm from bregma)', ...
+        'DV position (mm from bregma)', ...
+        'Azimuth angle (relative to lambda -> bregma)', ....
+        'Elevation angle (relative to horizontal)'};
 
-new_probe_position_input = inputdlg(prompt_text,'Set probe position',1);
-if any(cellfun(@isempty,new_probe_position_input))
-   error('Not all coordinates entered'); 
-end
-new_probe_position = cellfun(@str2num,new_probe_position_input);
+    new_probe_position_input = inputdlg(prompt_text,'Set probe position',1);
+    if any(cellfun(@isempty,new_probe_position_input))
+       error('Not all coordinates entered'); 
+    end
+    new_probe_position = cellfun(@str2num,new_probe_position_input);
 
-% Convert degrees to radians
-probe_angle_rad = (new_probe_position(4:5)/360)*2*pi;
+    % Convert degrees to radians
+    probe_angle_rad = (new_probe_position(4:5)/360)*2*pi;
 
-% Update the probe and trajectory reference
-ml_lim = xlim(gui_data.handles.axes_atlas);
-ap_lim = ylim(gui_data.handles.axes_atlas);
-dv_lim = zlim(gui_data.handles.axes_atlas);
-max_ref_length = norm([range(ap_lim);range(dv_lim);range(ml_lim)]);
-[y,x,z] = sph2cart(pi+probe_angle_rad(1),pi-probe_angle_rad(2),max_ref_length);
+    % Update the probe and trajectory reference
+    ml_lim = xlim(gui_data.handles.axes_atlas);
+    ap_lim = ylim(gui_data.handles.axes_atlas);
+    dv_lim = zlim(gui_data.handles.axes_atlas);
+    max_ref_length = norm([range(ap_lim);range(dv_lim);range(ml_lim)]);
+    [y,x,z] = sph2cart(pi+probe_angle_rad(1),pi-probe_angle_rad(2),max_ref_length);
 
-% Move probe reference (draw line through point and DV 0 with max length)
-probe_ref_top_ap = interp1(new_probe_position(3)+[0,z],new_probe_position(1)+[0,y],0,'linear','extrap');
-probe_ref_top_ml = interp1(new_probe_position(3)+[0,z],new_probe_position(2)+[0,x],0,'linear','extrap');
+    % Move probe reference (draw line through point and DV 0 with max length)
+    probe_ref_top_ap = interp1(new_probe_position(3)+[0,z],new_probe_position(1)+[0,y],0,'linear','extrap');
+    probe_ref_top_ml = interp1(new_probe_position(3)+[0,z],new_probe_position(2)+[0,x],0,'linear','extrap');
 
-probe_ref_top = [probe_ref_top_ml,probe_ref_top_ap,0];
-probe_ref_bottom = probe_ref_top + [x,y,z];
-probe_ref_vector = [probe_ref_top;probe_ref_bottom]';
+    probe_ref_top = [probe_ref_top_ml,probe_ref_top_ap,0];
+    probe_ref_bottom = probe_ref_top + [x,y,z];
+    probe_ref_vector = [probe_ref_top;probe_ref_bottom]';
 
-set(gui_data.handles.probe_ref_line,'XData',probe_ref_vector(1,:), ...
-    'YData',probe_ref_vector(2,:), ...
-    'ZData',probe_ref_vector(3,:));
+    set(gui_data.handles.probe_ref_line,'XData',probe_ref_vector(1,:), ...
+        'YData',probe_ref_vector(2,:), ...
+        'ZData',probe_ref_vector(3,:));
 
-% Move probe (lock endpoint, back up length of probe)
-probe_vector = [diff(probe_ref_vector,[],2)./norm(diff(probe_ref_vector,[],2))* ...
-    -gui_data.probe_length + new_probe_position([2,1,3]), ...
-    new_probe_position([2,1,3])];
-set(gui_data.handles.probe_line,'XData',probe_vector(1,:), ...
-    'YData',probe_vector(2,:),'ZData',probe_vector(3,:));
+    % Move probe (lock endpoint, back up length of probe)
+    probe_vector = [diff(probe_ref_vector,[],2)./norm(diff(probe_ref_vector,[],2))* ...
+        -gui_data.probe_length + new_probe_position([2,1,3]), ...
+        new_probe_position([2,1,3])];
+    set(gui_data.handles.probe_line,'XData',probe_vector(1,:), ...
+        'YData',probe_vector(2,:),'ZData',probe_vector(3,:));
 
-% Upload gui_data
-gui_data.probe_angle = (probe_angle_rad/(2*pi))*360;
-guidata(probe_atlas_gui, gui_data);
+    % Upload gui_data
+    gui_data.probe_angle = (probe_angle_rad/(2*pi))*360;
+    guidata(probe_atlas_gui, gui_data);
 
-% Update the slice and probe coordinates
-nte.update_slice(probe_atlas_gui);
-nte.update_probe_coordinates(probe_atlas_gui);
+    % Update the slice and probe coordinates
+    nte.update_slice(probe_atlas_gui);
+    nte.update_probe_coordinates(probe_atlas_gui);
 
 end
