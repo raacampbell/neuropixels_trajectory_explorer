b3cd5e48460ed9bcfe7b3e27bbc506dafc4cd665
Rob Campbell <git@raacampbell.com>
2023-01-04 14:53:28 +0000

    separate data processing from plotting in top-down

--- code/+nte/+utils/draw_top_down_ccf.m
+++ code/+nte/+utils/draw_top_down_ccf.m
@@ -1,90 +1,23 @@
-function draw_top_down_ccf(av, st)
+function  draw_top_down_ccf(plt_data)
     % Draw a top-down view of the Allen Atlas in coords from bregma
     %
-    % function nte.utils.draw_top_down_ccf(av, st)
+    % function nte.utils.draw_top_down_ccf(plt_data)
     %
     % Inputs [Optional]
-    % av - the Allen atlas volume
-    % st - the structure tree of the atlas
+    % plt_data - output of nte.utils.prep_data_for_top_down
     %
-    % The atlas (av) and the structure list (st) will be loaded if not supplied
-    % Supplying them will speed up appearance of the figure. see:
-    % nte.return_atlas and nte.return_structure_tree
+    % Supplying plt_data will speed up appearance of the figure.
     %
-    % Example function calls
-    % nte.utils.draw_top_down_ccf;
+    % Example function calls:
+    % td_data = nte.utils.prep_data_for_top_down
+    % nte.utils.draw_top_down_ccf(td_data) %allows for faster repeat calls
     %
-    % av = nte.return_atlas;
-    % nte.utils.draw_top_down_ccf(av)
-    %
+    % nte.utils.draw_top_down_ccf
 
-
-    if nargin < 1 || isempty(av)
-        av = nte.return_atlas;
-    end
-
-    if nargin < 2 || isempty(st)
-        st = nte.return_structure_tree;
-    end
-
-
-    % Get first brain pixel from top-down
-    [~,top_down_depth] = max(av>1, [], 2);
-    top_down_depth = squeeze(top_down_depth);
-
-
-    % Now get the annotation from the atlas at that point. The annotation tells us the brain area
-    [xx,yy] = meshgrid(1:size(top_down_depth,2), 1:size(top_down_depth,1));
-    top_down_annotation = reshape(av(sub2ind(size(av),yy(:),top_down_depth(:),xx(:))), size(av,1), size(av,3));
-
-
-    % The brain areas present in the annotation (so we search only these)
-    used_areas = unique(top_down_annotation(:));
-
-    % Restrict to only cortical areas
-    structure_id_path = cellfun(@(x) textscan(x(2:end),'%d', 'delimiter',{'/'}),st.structure_id_path);
-
-    ctx_path = [997,8,567,688,695,315];
-    ctx_idx = find(cellfun(@(id) length(id) > length(ctx_path) & ...
-        all(id(min(length(id),length(ctx_path))) == ctx_path(min(length(id),length(ctx_path)))),structure_id_path));
-
-    plot_areas = intersect(used_areas,ctx_idx);
-
-
-
-    % Get outlines and names of all cortical areas visible from top-down
-    dorsal_cortical_areas = ...
-        struct('boundaries',cell(size(plot_areas)),'names',cell(size(plot_areas)));
-
-    % Tidy names
-    for curr_area_idx = 1:length(plot_areas)
-        dorsal_cortical_areas(curr_area_idx).boundaries = ...
-            bwboundaries(top_down_annotation == plot_areas(curr_area_idx));
-
-        t_name = st.safe_name(plot_areas(curr_area_idx));
-
-        % (dorsal areas are all "layer 1" - remove that)
-        t_name = regexprep(t_name,'..ayer 1','');
-
-        % Remove the term "area", as it's obvious and makes the names long
-        t_name = regexprep(t_name,'area','');
-        t_name = regexprep(t_name,'  ',' ');
-
-        % Replace "Primary" with 1' and "Secondary" with 2'
-        t_name = regexprep(t_name,'Primary','1''');
-        t_name = regexprep(t_name,'Secondary','2''');
-
-        dorsal_cortical_areas(curr_area_idx).names  = t_name;
-    end
-
-    % Convert boundary units to stereotaxic coordinates in mm
-    bregma = [540,44,570];
-    boundaries_stereotax = cellfun(@(area) cellfun(@(outline) ...
-        [bregma(1)-outline(:,1),outline(:,2)-bregma(3)]/100,area,'uni',false), ...
-        {dorsal_cortical_areas.boundaries},'uni',false);
-    [dorsal_cortical_areas.boundaries_stereotax] = boundaries_stereotax{:};
-
-
+    if nargin<1
+        plt_data = nte.utils.prep_data_for_top_down;
+    end 
+    
     %% Draw top-down cortical areas (in mm)
 
     brain_color = 'k';
@@ -106,15 +39,15 @@ function draw_top_down_ccf(av, st)
 
     % Draw cortical boundaries
     cellfun(@(x) cellfun(@(x) plot(x(:,2),x(:,1),'color',brain_color),x,'uni',false), ...
-        {dorsal_cortical_areas.boundaries_stereotax},'uni',false);
+        {plt_data.dorsal_cortical_areas.boundaries_stereotax},'uni',false);
 
     % Write area labels (left hemisphere)
     cellfun(@(x,y) text('Position',fliplr(mean(x{1}+0.05,1)),'String',y, 'Color', [0,0,0.5]), ...
-        {dorsal_cortical_areas.boundaries_stereotax}, ...
-        {dorsal_cortical_areas.names});
+        {plt_data.dorsal_cortical_areas.boundaries_stereotax}, ...
+        {plt_data.dorsal_cortical_areas.names});
 
     cellfun(@(x,y) plot(mean(x{1}(:,2)),mean(x{1}(:,1)),'.b', 'MarkerSize', 10), ...
-        {dorsal_cortical_areas.boundaries_stereotax});
+        {plt_data.dorsal_cortical_areas.boundaries_stereotax});
 
     % Plot bregma
     plot(0,0,'.','color',bregma_color','MarkerSize',20);
---
+++ code/+nte/+utils/prep_data_for_top_down.m
@@ -0,0 +1,102 @@
+function d
+    % Generate data needed for making the top-down plot
+    % 
+    % function td_data = prep_data_for_top_down(av, st)
+    %
+    % Purpose
+    % Generating and processing the data for making the top-down plot is 
+    % fairly time consuming. This function pre-computes the data.
+    % In addition, the pre-computed data are much smaller than the original
+    % and can be cached easily for re-loading with minimal footprint
+    %
+    % Example function calls
+    % tdd = nte.utils.prep_data_for_top_down;
+    %
+    % av = nte.return_atlas;
+    % tdd = nte.utils.prep_data_for_top_down(av)
+    %
+    % Rob Campbell -- SWC 2023
+
+
+
+    if nargin < 1 || isempty(av)
+        av = nte.return_atlas;
+    end
+
+    if nargin < 2 || isempty(st)
+        st = nte.return_structure_tree;
+    end
+
+
+    % Get first brain pixel from top-down
+    [~,top_down_depth] = max(av>1, [], 2);
+    top_down_depth = squeeze(top_down_depth);
+
+
+    % Now get the annotation from the atlas at that point. The annotation tells us the brain area
+    [xx,yy] = meshgrid(1:size(top_down_depth,2), 1:size(top_down_depth,1));
+    top_down_annotation = reshape(av(sub2ind(size(av),yy(:),top_down_depth(:),xx(:))), size(av,1), size(av,3));
+
+
+
+    % The brain areas present in the annotation (so we search only these)
+    used_areas = unique(top_down_annotation(:));
+
+    % Restrict to only cortical areas
+    structure_id_path = cellfun(@(x) textscan(x(2:end),'%d', 'delimiter',{'/'}),st.structure_id_path);
+
+    ctx_path = [997,8,567,688,695,315];
+    ctx_idx = find(cellfun(@(id) length(id) > length(ctx_path) & ...
+        all(id(min(length(id),length(ctx_path))) == ctx_path(min(length(id),length(ctx_path)))),structure_id_path));
+
+    plot_areas = intersect(used_areas,ctx_idx);
+
+
+
+
+    % Get outlines and names of all cortical areas visible from top-down
+    dorsal_cortical_areas = struct(...
+            'boundaries', cell(size(plot_areas)), ...
+            'names',cell(size(plot_areas)) ...
+            );
+
+    % Tidy names
+    for curr_area_idx = 1:length(plot_areas)
+        dorsal_cortical_areas(curr_area_idx).boundaries = ...
+            bwboundaries(top_down_annotation == plot_areas(curr_area_idx));
+
+        t_name = st.safe_name(plot_areas(curr_area_idx));
+
+        % (dorsal areas are all "layer 1" - remove that)
+        t_name = regexprep(t_name,'..ayer 1','');
+
+        % Remove the term "area", as it's obvious and makes the names long
+        t_name = regexprep(t_name,'area','');
+        t_name = regexprep(t_name,'  ',' ');
+
+        % Replace "Primary" with 1' and "Secondary" with 2'
+        t_name = regexprep(t_name,'Primary','1''');
+        t_name = regexprep(t_name,'Secondary','2''');
+
+        dorsal_cortical_areas(curr_area_idx).names  = t_name;
+    end
+    
+
+
+    % Convert boundary units to stereotaxic coordinates in mm
+    bregma = [540,44,570];
+
+    boundaries_stereotax = cellfun(@(area) cellfun(@(outline) ...
+        [bregma(1)-outline(:,1),outline(:,2)-bregma(3)]/100,area,'uni',false), ...
+        {dorsal_cortical_areas.boundaries},'uni',false);
+
+    [dorsal_cortical_areas.boundaries_stereotax] = boundaries_stereotax{:};
+
+
+    % Make the output structure
+    td_data.dorsal_cortical_areas = dorsal_cortical_areas;
+    td_data.plot_areas = plot_areas;
+    td_data.top_down_annotation = top_down_annotation;
+
+
+
